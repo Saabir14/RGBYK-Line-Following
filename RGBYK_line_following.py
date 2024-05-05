07d11f452b43f5529da83cd7c4e47c8a84f55b29
Saabir Ahmed <64490729+Saabir14@users.noreply.github.com>
2024-05-05 23:50:12 +0100

    added symbol recognition to line following

--- RGBYK_line_following.py
+++ RGBYK_line_following.py
@@ -3,6 +3,9 @@ import numpy as np
 from smbus2 import SMBus
 import struct
 
+import os
+from symbol_recognition import symbol_recognition
+
 # Initialize the I2C bus.
 bus = SMBus(1)
 
@@ -11,19 +14,30 @@ device_address = 0x04
 
 # Define the color boundaries.
 color_bounds = {
-    'r': (((0, 120, 20), (7, 255, 255)), ((176, 102, 20), (180, 255, 255))),  # red
-    'g': ((35, 102, 20), (80, 255, 255)),  # green
-    'b': ((89, 102, 20), (125, 255, 255)),  # blue
-    'y': ((17, 102, 20), (34, 255, 255)),  # yellow
-    'k': ((0, 0, 0), (180, 102, 120)),  # black
+    'r': (np.array([0, 120, 20]), np.array([7, 255, 255]), (np.array([176, 102, 20]), np.array([180, 255, 255]))),  # red
+    'g': (np.array([35, 102, 20]), np.array([80, 255, 255])),  # green
+    'b': (np.array([89, 102, 20]), np.array([125, 255, 255])),  # blue
+    'y': (np.array([17, 102, 20]), np.array([34, 255, 255])),  # yellow
+    'k': (np.array([0, 0, 0]), np.array([180, 102, 120])),  # black
+    'p': (np.array([100, 40, 0]), np.array([160, 255, 255])), # pink
 }
+
+# Load the images for symbol recognition.
+source_file = "symbols"
+images = {}
+for image_file_path in os.listdir(source_file):
+    image = cv2.imread(os.path.join(source_file, image_file_path))
+    if image is not None:
+        images[image_file_path] = cv2.inRange(cv2.cvtColor(image, cv2.COLOR_BGR2HSV), (100, 40, 0), (160, 255, 255))
+
+symbol_detection_threshold = .2
 
 def RGBYK_line_following():
     # Initialize the camera.
     cap = cv2.VideoCapture(0)
 
     # Ask the user to choose a color.
-    choose_color = input("Select which colour you wish the car to follow\nb=Blue | k=Black | r=Red | y=Yellow | g=Green\n")
+    color = 'k'
 
     # Create a weight matrix where each column is filled with the column index minus half the frame width.
     weights = np.arange(-cap.get(cv2.CAP_PROP_FRAME_WIDTH) // 2, cap.get(cv2.CAP_PROP_FRAME_WIDTH) // 2)
@@ -35,23 +49,26 @@ def RGBYK_line_following():
 
         if not ret:
             print("Failed to capture a frame.")
-            break
+            continue
 
         # Convert the frame to HSV.
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+        # Check if symbol recognition is needed.
+        pink_mask = np.bitwise_or.reduce([cv2.inRange(hsv, np.array(lower), np.array(upper)) for lower, upper in color_bounds['p']])
+        if np.sum(pink_mask) / (255 * pink_mask.size) > symbol_detection_threshold:
+            color = symbol_recognition(frame, images, .75)
+
         # Apply the color bounds.
-        mask = np.bitwise_or.reduce([cv2.inRange(hsv, np.array(lower), np.array(upper)) for lower, upper in color_bounds[choose_color]])
+        mask = np.bitwise_or.reduce([cv2.inRange(hsv, np.array(lower), np.array(upper)) for lower, upper in color_bounds[color]])
 
-        # Calculate the sum of the product of the non-zero pixels in the mask and the weight matrix.
-        sum = np.sum(mask[mask != 0] * weights[mask != 0])
-
-        w = sum / total if total != 0 else 0
+        # Find the weighted average.
+        # w = np.sum(mask[mask != 0] * weights[mask != 0]) / total
+        w = np.dot(mask[mask != 0], weights[mask != 0]) / total
 
         # Write the weighted average to the device.
         bus.write_i2c_block_data(device_address, 0, struct.pack('d', w))
-        '''
-        If error occurs, such as bytes sent the wrong way, try the following code instead:
+        ''' If error occurs, such as bytes sent the wrong way, try the following code instead:
         bus.write_i2c_block_data(device_address, 0, list(struct.pack('<d', w)))
             OR
         bus.write_i2c_block_data(device_address, 0, [ord(c) for c in struct.pack('>d', w)])
@@ -87,6 +104,7 @@ def RGBYK_line_following():
             }
         }
         '''
+        # if condition_to_break: break
 
     # Release the camera and close all windows.
     cap.release()
